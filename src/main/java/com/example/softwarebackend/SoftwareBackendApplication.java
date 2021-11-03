15502e7b5b3805bf8ebafaadd381ae456cddb2f7
rpek99 <rstmpk28@gmail.com>
2021-11-04 01:02:01 +0300

    first commit

---
+++ src/main/java/com/example/softwarebackend/SoftwareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.softwarebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareBackendApplication.class, args);
+	}
+
+}
